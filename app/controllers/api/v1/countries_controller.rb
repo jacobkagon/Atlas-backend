0d4bd09a85e73685b743317d6b1b95988f31ead2
purisquiri <mau.dorigo@outlook.es>
2020-12-26 17:01:28 -0500

    user and country routes

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::CountriesController < ApplicationController
+    before_action :set_country, only: [:show, :update]
+
+    def index
+        countries = Country.all
+
+        render json: countries.to_json(country_serializer_options)
+    end 
+
+    def show_exceptions
+        country = Country.find(params[:id])
+        render json: countries.to_json(country_serializer_options)
+    end
+
+    private
+
+    def set_country
+        @country = Country.find(params[:id])
+    end
+
+    def country_params
+        params.require(:country).permit(:name, :country_code)
+    end
+
+    def country_serializer_options() { 
+        :include => { 
+            :reviews => { 
+                :except => [:created_at, :update_at]
+             }, 
+            :favorites => { 
+                :except => [:created_at, :update_at]
+             }
+         },
+         :except => [:created_at, :updated_at]
+     }
+    end
+    
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,6 @@
 class Country < ApplicationRecord
+    has_many :reviews
+    has_many :users, through: :reviews
+    has_many :favorites
+    has_many :users, through: :favorites
 end
