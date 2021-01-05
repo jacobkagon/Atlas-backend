96a29a35dc826c8679c40bcd9b066d90f5896f15
purisquiri <mau.dorigo@outlook.es>
2021-01-05 14:13:59 -0500

    reviews serializer

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ReviewsController < ApplicationController
  
     def index
         @reviews = Review.all
-        render json: @review
+        render json: @reviews
     end
 
     def show
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :body, :user_id, :country_id, :rating
+  attributes :id, :body, :user_id, :country_id, :rating, :user, :created_at, :updated_at
 end
