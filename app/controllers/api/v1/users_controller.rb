2a1d501aec53dfba79aaf97f5a724d0f6e4f0614
JakesProjects1 <jacob@kagon.net>
2021-02-09 13:08:50 -0500

    added procfile to Atlas backend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-    skip_before_action :authorized, only: [:show, :create]
+    skip_before_action :authorized, only: [:index, :show, :create]
 
     def index
         @users = User.all
